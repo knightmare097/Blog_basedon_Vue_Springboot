34e0451a6788af7aad433cd1f9e114f85a0c8a2f
knightmare <1339746266@qq.com>
2023-03-01 20:25:48 +0800

    test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int i=0;
+        i+=1;
+        System.out.println(i);
     }
 }
+
